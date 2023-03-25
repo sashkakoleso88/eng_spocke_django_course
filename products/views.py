39b261e91ff1ae1e5dfeec5e365627ec87aed357
oleksandr <sashkakoleso88@gmail.com>
2023-03-25 16:21:56 +0200

    add views (index, products), add templates for views + static dir

--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Функции = контроллеры = обработчики запросов
+
+
+def index(request):
+    return render(
+        request=request,
+        template_name='products/index.html',
+    )
+
+
+def products(request):
+    return render(
+        request=request,
+        template_name='products/products.html',
+    )
